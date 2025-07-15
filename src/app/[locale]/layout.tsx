2b490274f2ef6ae73dd4416f24fb885308efb580
Сармат Цомаев <4istopodely@gmail.com>
2025-07-15 12:39:12 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,29 +1,11 @@
+'use client';
 import type {Metadata} from 'next';
 import { Toaster } from "@/components/ui/toaster";
 import '../globals.css';
-import { Oswald, Roboto_Condensed } from 'next/font/google';
-import { getCurrentLocale } from '@/locales/server';
 import { I18nProviderClient } from '@/locales/client';
 import LanguageSwitcher from '@/components/language-switcher';
 
-const oswald = Oswald({
-  subsets: ['latin'],
-  variable: '--font-oswald',
-  weight: ['400', '700'],
-});
-
-const robotoCondensed = Roboto_Condensed({
-  subsets: ['latin'],
-  variable: '--font-roboto-condensed',
-  weight: ['400'],
-});
-
-export const metadata: Metadata = {
-  title: 'helper for chiselling',
-  description: 'Create pixel art schematics for Vintage Story',
-};
-
-export default async function RootLayout({
+export default function RootLayout({
   children,
   params: { locale }
 }: Readonly<{
@@ -32,17 +14,11 @@ export default async function RootLayout({
 }>) {
   return (
     <I18nProviderClient locale={locale}>
-      <html className="dark">
-        <head>
-        </head>
-        <body className={`${oswald.variable} ${robotoCondensed.variable} font-body antialiased`}>
-          <div className="absolute top-4 right-4 z-10">
-            <LanguageSwitcher />
-          </div>
-          {children}
-          <Toaster />
-        </body>
-      </html>
+        <div className="absolute top-4 right-4 z-10">
+          <LanguageSwitcher />
+        </div>
+        {children}
+        <Toaster />
     </I18nProviderClient>
   );
 }
--- src/locales/client.ts
+++ src/locales/client.ts
@@ -1,8 +1,28 @@
 'use client';
 
 import { createI18nClient } from 'next-international/client';
+import { Oswald, Roboto_Condensed } from 'next/font/google';
 
-export const { useI18n, useScopedI18n, I18nProviderClient, useChangeLocale, useCurrentLocale } = createI18nClient({
-  en: () => import('./en'),
-  ru: () => import('./ru'),
+const oswald = Oswald({
+  subsets: ['latin'],
+  variable: '--font-oswald',
+  weight: ['400', '700'],
 });
+
+const robotoCondensed = Roboto_Condensed({
+  subsets: ['latin'],
+  variable: '--font-roboto-condensed',
+  weight: ['400'],
+});
+
+
+export const { useI18n, useScopedI18n, I18nProviderClient, useChangeLocale, useCurrentLocale } = createI18nClient(
+  {
+    en: () => import('./en'),
+    ru: () => import('./ru'),
+  },
+  {
+    htmlClassName: `${oswald.variable} ${robotoCondensed.variable} font-body antialiased dark`,
+    // The middleware will handle the locale detection and redirection.
+  },
+);
