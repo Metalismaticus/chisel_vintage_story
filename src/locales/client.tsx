8c92563a09b70a6fb7b5917a9c455b6c7124a4ae
Сармат Цомаев <4istopodely@gmail.com>
2025-07-15 11:46:30 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/locales/client.tsx
+++ src/locales/client.tsx
@@ -3,8 +3,8 @@ import { createI18nClient } from 'next-international/client';
 
 export const { useI18n, useScopedI18n, I18nProvider, useChangeLocale, useCurrentLocale } = createI18nClient(
   {
-    en: () => import('./en'),
-    ru: () => import('./ru'),
+    en: () => import('./en').then(m => m.default),
+    ru: () => import('./ru').then(m => m.default),
   },
   {
     // Uncomment to set the default locale
