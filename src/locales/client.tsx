f0c8c59d0aae11aea9f387d163a6b2ff3495a356
Сармат Цомаев <4istopodely@gmail.com>
2025-07-15 11:51:41 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/locales/client.tsx
+++ src/locales/client.tsx
@@ -2,7 +2,8 @@
 
 import { createI18nClient } from 'next-international/client';
 
-export const { useI18n, useScopedI18n, I18nProvider, useChangeLocale, useCurrentLocale } = createI18nClient({
-  en: () => import('./en').then(m => m.default),
-  ru: () => import('./ru').then(m => m.default),
-});
+export const { useI18n, useScopedI18n, I18nProviderClient, useChangeLocale, useCurrentLocale: useCurrentLocaleClient } =
+  createI18nClient({
+    en: () => import('./en'),
+    ru: () => import('./ru'),
+  });
--- src/locales/provider.tsx
+++ src/locales/provider.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { I18nProvider } from 'next-international/client';
+import { I18nProviderClient } from '@/locales/client';
 import type { ReactNode } from 'react';
 
-export default function I18nProviderClient({
+export default function I18nProvider({
   locale,
   children,
 }: {
@@ -11,16 +11,13 @@ export default function I18nProviderClient({
   children: ReactNode;
 }) {
   return (
-    <I18nProvider
+    <I18nProviderClient
       locale={locale}
       fallback={
-        // Optional: use `de` locale as fallback
         <div>loading...</div>
       }
-      // Optional: disable on-demand loading
-      // loadOnDemand={false}
     >
       {children}
-    </I18nProvider>
+    </I18nProviderClient>
   );
 }
